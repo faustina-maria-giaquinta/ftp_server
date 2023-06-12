2c3ec62d6c27f5494f80262fe41901a1da96001a
Faustina Maria Giaquinta <faustina-maria.giaquinta@laplateforme.io>
2023-06-12 09:56:57 +0200

    modifié le script pour installer directement à partir du repo

--- server_install.sh
+++ server_install.sh
@@ -3,5 +3,5 @@
 apt update && apt upgrade -y
 apt install proftpd git -y
 
-mv proftpd.conf /etc/proftpd/ && service proftpd restart
-mv sshd_config /etc/ssh/ && service sshd start && service ssh start
+wget -P /etc/proftpd/ https://raw.githubusercontent.com/faustina-maria-giaquinta/ftp_server/main/proftpd.conf && service proftpd restart
+wget -P /etc/ssh/ https://raw.githubusercontent.com/faustina-maria-giaquinta/ftp_server/main/sshd_config && service sshd start && service ssh start
